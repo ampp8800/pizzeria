3085012d7cbad0dc613d3c8c8954e7ab287e9a73
EitherNor <torerotheorc@gmail.com>
2021-10-31 22:53:39 +0300

    Revert "added warehouse and cook"

--- src/main/java/com/ampp8800/pizzeria/Pizzeria.java
+++ src/main/java/com/ampp8800/pizzeria/Pizzeria.java
@@ -1,20 +1,12 @@
 package com.ampp8800.pizzeria;
 
 public class Pizzeria {
-    final static int NUMBER_OF_COOKS = 3;
 
     public static void main(String[] args) {
-        ProgramsMethods.printTotalOrders();
-
-        Warehouse warehouse = Warehouse.getInstance();
-        warehouse.fillWithIngredients();
-
+        OrderQueueWrapper orderQueueWrapper = OrderQueueWrapper.getInstance();
+        orderQueueWrapper.printTotalOrders();
         OrderGenerator orderGenerator = new OrderGenerator();
         orderGenerator.start();
 
-        for (int i = 0; i < NUMBER_OF_COOKS; i++) {
-            Cook cook = new Cook();
-            cook.start();
-        }
     }
 }
