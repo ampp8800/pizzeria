dd70acfcd0eca6a4bddfa850256657f3c618f6d7
dmitry <fireheart.dev@gmail.com>
2021-11-01 10:54:29 +0300

    Revert "Revert "added warehouse and cook""
    This reverts commit 3085012d

--- src/main/java/com/ampp8800/pizzeria/Pizzeria.java
+++ src/main/java/com/ampp8800/pizzeria/Pizzeria.java
@@ -1,12 +1,20 @@
 package com.ampp8800.pizzeria;
 
 public class Pizzeria {
+    final static int NUMBER_OF_COOKS = 3;
 
     public static void main(String[] args) {
-        OrderQueueWrapper orderQueueWrapper = OrderQueueWrapper.getInstance();
-        orderQueueWrapper.printTotalOrders();
+        ProgramsMethods.printTotalOrders();
+
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.fillWithIngredients();
+
         OrderGenerator orderGenerator = new OrderGenerator();
         orderGenerator.start();
 
+        for (int i = 0; i < NUMBER_OF_COOKS; i++) {
+            Cook cook = new Cook();
+            cook.start();
+        }
     }
 }
